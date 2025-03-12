0ebaa5d487903919d3dde74fe233381fc91b59d0
hbz567 <hasanzahid56763333@gmail.com>
2025-03-12 14:31:53 +0500

    Fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -28,7 +28,7 @@ cart.forEach(cartItem => {
                     ${product.name}
                 </div>
                 <div class="product-price">
-                    $${(product.priceCents / 100).toFixed(2)}
+                    $${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                     <span>
@@ -44,48 +44,48 @@ cart.forEach(cartItem => {
                 </div>
 
                 <div class="delivery-options">
-                <div class="delivery-options-title">
-                    Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                    <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                    <div>
-                    <div class="delivery-option-date">
-                        Tuesday, June 21
+                    <div class="delivery-options-title">
+                        Choose a delivery option:
                     </div>
-                    <div class="delivery-option-price">
-                        FREE Shipping
+                    <div class="delivery-option">
+                        <input type="radio" checked
+                        class="delivery-option-input"
+                        name="delivery-option-${product.id}">
+                        <div>
+                        <div class="delivery-option-date">
+                            Tuesday, June 21
+                        </div>
+                        <div class="delivery-option-price">
+                            FREE Shipping
+                        </div>
+                        </div>
                     </div>
+                    <div class="delivery-option">
+                        <input type="radio"
+                        class="delivery-option-input"
+                        name="delivery-option-${product.id}">
+                        <div>
+                        <div class="delivery-option-date">
+                            Wednesday, June 15
+                        </div>
+                        <div class="delivery-option-price">
+                            $4.99 - Shipping
+                        </div>
+                        </div>
                     </div>
-                </div>
-                <div class="delivery-option">
-                    <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                    <div>
-                    <div class="delivery-option-date">
-                        Wednesday, June 15
+                    <div class="delivery-option">
+                        <input type="radio"
+                        class="delivery-option-input"
+                        name="delivery-option-${product.id}">
+                        <div>
+                        <div class="delivery-option-date">
+                            Monday, June 13
+                        </div>
+                        <div class="delivery-option-price">
+                            $9.99 - Shipping
+                        </div>
+                        </div>
                     </div>
-                    <div class="delivery-option-price">
-                        $4.99 - Shipping
-                    </div>
-                    </div>
-                </div>
-                <div class="delivery-option">
-                    <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                    <div>
-                    <div class="delivery-option-date">
-                        Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                        $9.99 - Shipping
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
