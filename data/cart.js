99b72eb86b2fad73c864605b18a76b05c253f084
hbz567 <hasanzahid56763333@gmail.com>
2025-03-12 12:44:47 +0500

    Move addToCart in cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,17 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let inCart = false;
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+        inCart = true;
+        cartItem.quantity += 1;
+        }
+    });
+
+    inCart || cart.push({
+        productId,
+        quantity: 1
+    });
+}
