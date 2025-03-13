6832c4f5d0a40be59bb8a3e9022735c873b737a3
hbz567 <hasanzahid56763333@gmail.com>
2025-03-13 06:36:06 +0500

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,4 @@
-export const cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export function addToCart(productId) {
     let inCart = false;
@@ -20,6 +14,8 @@ export function addToCart(productId) {
         productId,
         quantity: 1
     });
+
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -28,4 +24,9 @@ export function removeFromCart(productId) {
             cart.splice(index, 1);
         }
     });
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
