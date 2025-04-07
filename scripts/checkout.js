8937e25d3325347dacfc40723d2ace50bc5a6b2e
hbz567 <hasanzahid56763333@gmail.com>
2025-04-07 09:11:25 +0500

    18h-18j

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,24 +1,17 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 async function loadPage() {
     try {
-        // throw 'error1';
-
-        await loadProductsFetch();
-
-        const value = await new Promise((resolve, reject) => {
-            // throw 'error2';
-            loadCart(() => {
-                // reject('error3');
-                resolve('value2');
-            });
-        });
-
+        await Promise.all([
+            loadProductsFetch(),
+            loadCartFetch()
+        ]);
+        
     } catch (error) {
         console.log('Unexpected Error. Please try again later. ' + error);
     }
