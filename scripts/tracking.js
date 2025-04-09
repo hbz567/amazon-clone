c1dc0b88433a330e54e63105a2e513e21afc04f2
hbz567 <hasanzahid56763333@gmail.com>
2025-04-09 17:21:38 +0500

    18o

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -40,21 +40,42 @@ async function loadPage() {
             <img class="product-image" src="../${product.image}">
 
             <div class="progress-labels-container">
-            <div class="progress-label">
+            <div class="progress-label js-preparing-label">
                 Preparing
             </div>
-            <div class="progress-label current-status">
+            <div class="progress-label js-shipped-label">
                 Shipped
             </div>
-            <div class="progress-label">
+            <div class="progress-label js-delivered-label">
                 Delivered
             </div>
             </div>
 
             <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+                <div class="progress-bar js-progress-bar"></div>
             </div>
         </div>
     `;
+
+    const orderTime = new Date(order.orderTime);
+    const deliveryTime = new Date(orderItem.estimatedDeliveryTime);
+    const currentTime = new Date();
+
+    const progressPercentage = Math.round(
+        ((currentTime - orderTime) / (deliveryTime - orderTime)) * 10000
+    );
+
+    let status = '';
+
+    if (progressPercentage <= 49) {
+        status = 'preparing';
+    } else if (progressPercentage <= 99) {
+        status = 'shipped';
+    } else {
+        status = 'delivered';
+    }
+
+    document.querySelector(`.js-${status}-label`).classList.add('current-status');
+    document.querySelector('.js-progress-bar').style.width = progressPercentage + '%';
 }
 loadPage();
